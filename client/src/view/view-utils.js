f25770db81e7ccd6beed73a1955d4cf4ac91e6a4
Shankhadeep <shankhadeep17.sb@gmail.com>
2022-04-10 12:04:51 +0530

    feat(client): adds tailwind, vite

--- client/src/view/view-utils.js
+++ client/src/view/view-utils.js
@@ -1,6 +1,7 @@
 function createListElement(messageObj){
     const {owner, text, id} = messageObj;
     const element = document.createElement('li');
+    element.classList.add('p-2','bg-slate-200', 'rounded', 'my-1', 'w-fit')
     element.innerHTML = `<strong>${owner}: </strong> <span>${text}</span>`;
     return element
 }
