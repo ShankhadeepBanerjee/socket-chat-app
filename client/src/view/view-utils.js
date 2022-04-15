b0295567b069b01e398141750270d73b7abbc6ca
Shankhadeep <shankhadeep17.sb@gmail.com>
2022-04-15 23:18:57 +0530

    feat(client): makes the chatlist a flex-col

--- client/src/view/view-utils.js
+++ client/src/view/view-utils.js
@@ -1,7 +1,11 @@
 function createListElement(messageObj){
     const {owner, text, id} = messageObj;
     const element = document.createElement('li');
-    element.classList.add('p-2','bg-slate-200', 'rounded', 'my-1', 'w-fit', 'max-w-[80%]', 'overflow-hidden', 'text-wrapp-circle', 'transition', 'ease-in')
+    element.classList.add('p-2','bg-slate-200', 'rounded', 
+                            'grow-1', 'shrink-0',
+                            'my-1', 'w-fit', 'max-w-[80%]', 
+                            'overflow-hidden', 'text-wrapp-circle', 
+                            'transition', 'ease-in')
     element.innerHTML = `
                         <strong>${owner}: </strong> 
                         <span class="overflow-hidden break-words">${text}</span>
